d8d4f58521d489f1a6b9153a74fa2d5e0ef0a65d
stevecass <steven@mumsnet.com>
2015-04-17 11:33:16 -0400

    Add a home controller and some cat/dog resources

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,3 @@
+class HomeController < ApplicationController
+  #We just render app/views/home/index.html.erb
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :dogs, only:[:index, :show, :create]
+  resources :cats
+  root 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
