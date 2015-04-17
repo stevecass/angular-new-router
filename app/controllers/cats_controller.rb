c62f748c870ae4deaef5e26e3b682404ce586214
stevecass <steven@mumsnet.com>
2015-04-17 16:21:06 -0400

    Put minimal security around the rails resources

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,7 +1,12 @@
 class CatsController < ApplicationController
-def index
-    @cats = Cat.all
-    render json: @cats
+  def index
+    cats = Cat.all
+    render json: cats
+  end
+
+  def show
+    cat = Cat.find(params[:id])
+    render json: cat
   end
 
   def create
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,4 @@
 class HomeController < ApplicationController
   #We just render app/views/home/index.html.erb
+  skip_before_action :require_login
 end
