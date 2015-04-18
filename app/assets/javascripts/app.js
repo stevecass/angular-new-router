2ef0088e231d5082f99e76c47184b4b125d32817
Steven Cassidy <steven@mumsnet.com>
2015-04-18 11:39:48 -0400

    Add a sample component.

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -4,6 +4,7 @@ angular.module('an-app').controller('AppController', ['$router', AppController])
 function AppController ($router) {
   $router.config([
     {path: '/', component: 'home' },
+    {path: '/sample', component: 'sample' },
     {path: '/detail/:id', component: 'detail' }
   ]);
 }
--- app/assets/javascripts/controllers.js
+++ app/assets/javascripts/controllers.js
@@ -17,3 +17,7 @@ angular.module('an-app')
     }.bind(this));
 
 }]);
+
+angular.module('an-app').controller('SampleController', [function(){
+  this.user_message = "I am user_message from SampleController. SampleController declares no dependencies.";
+}]);
