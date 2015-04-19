a3819a41f01e2e5f0f5c7f52107553ee4b2aebe0
Steven Cassidy <steven@mumsnet.com>
2015-04-19 09:58:11 -0400

    Include the rails CSRF token in angular requests

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,4 +1,11 @@
 angular.module('an-app', ['ngNewRouter', 'ngResource']);
+
+//Tell angular to use the CSRF token rails provides on the page
+angular.module('an-app').config(function($httpProvider) {
+  $httpProvider.defaults.headers.common['X-CSRF-Token'] =
+    $('meta[name=csrf-token]').attr('content');
+});
+
 angular.module('an-app').controller('AppController', ['$router', AppController]);
 
 function AppController ($router) {
