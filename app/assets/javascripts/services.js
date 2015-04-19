258fc63e7396b2723af2a83bfbced13eef2a43d1
Steven Cassidy <steven@mumsnet.com>
2015-04-19 09:58:27 -0400

    Enhance the cat service to support updates

--- app/assets/javascripts/services.js
+++ app/assets/javascripts/services.js
@@ -1,5 +1,14 @@
+/*
+  The {id: '@id'} means use the id of the passed object to make the url.
+  The extra update method is needed because gnResource by default provides only
+  .save which is a POST. Rails expects post for create and PUT/PATCH for update.
+*/
 angular.module('an-app').factory('CatService', function($resource) {
-  return $resource('/cats/:id'); // Note the full endpoint address
+  return $resource('/cats/:id', {id: '@id'},
+    {
+      'update': { method:'PUT' }
+    }
+  ); 
 });
 
 
