002550d00c599e3ea30de1c3172dc67bb1ac7234
Steven Cassidy <steven.cassidy@gmail.com>
2015-11-07 19:27:09 -0500

    Have AppController call AuthService for current user

--- app/assets/javascripts/controllers.js
+++ app/assets/javascripts/controllers.js
@@ -1,12 +1,23 @@
-angular.module('an-app')
-  .controller('HomeController', [ 'CatService', function (CatService) {
-    this.name = 'I am the home controller';
+angular.module('an-app').controller('AppController', ['AuthService', '$router', function AppController (AuthService, $router) {
+  $router.config([
+    {path: '/', component: 'home' },
+    {path: '/sample', component: 'sample' },
+    {path: '/detail/:id', component: 'detail' }
+  ]);
 
+  AuthService.getCurrentUser()
+  .then(function(response) {
+    this.rails_current_user = response.data;
+  }.bind(this));
+}]);
+
+angular.module('an-app')
+  .controller('HomeController', ['CatService', function (CatService) {
+    this.name = 'I am the home controller';
     CatService.query(function(data){
       this.cats = data;
     }.bind(this));
-
-}]);
+  }]);
 
 angular.module('an-app')
   .controller('DetailController', ['$routeParams', 'CatService', function ($routeParams, CatService) {
@@ -16,10 +27,11 @@ angular.module('an-app')
     }.bind(this));
 
     this.saveKitty = function() {
-      CatService.update(this.cat), function(data) {
+      CatService.update(this.cat)
+      .then(function(data) {
         this.cat = data;
-      }
-    }
+      }.bind(this));
+    };
 
 }]);
 
