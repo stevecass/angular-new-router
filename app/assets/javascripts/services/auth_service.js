8bfbfcfffd9ca2d4d46b8070cf682264a02b0be9
Steven Cassidy <steven.cassidy@gmail.com>
2015-11-07 20:44:40 -0500

    Move ng services into separate files

--- app/assets/javascripts/services/auth_service.js
+++ app/assets/javascripts/services/auth_service.js
@@ -1,25 +1,3 @@
-/*
-  The {id: '@id'} means use the id of the passed object to make the url.
-  The extra update method is needed because ngResource by default provides only
-  .save which is a POST. Rails expects post for create and PUT/PATCH for update.
-*/
-angular.module('an-app').factory('CatService', function($resource) {
-  return $resource('/cats/:id', {id: '@id'},
-    {
-      'update': { method:'PUT' }
-    }
-  );
-});
-
-angular.module('an-app').service('Session', function () {
-  this.create = function (userData) {
-    this.userData = userData;
-  };
-  this.destroy = function () {
-    this.userData = null;
-  };
-});
-
 angular.module('an-app').factory('AuthService', function(Session, $http) {
   var authService = {};
 
@@ -59,4 +37,3 @@ angular.module('an-app').factory('AuthService', function(Session, $http) {
 
   return authService;
 });
-
