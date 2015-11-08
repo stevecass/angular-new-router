3899dc4fa49d6d9a54515d53e605b2853da7cdac
Steven Cassidy <steven.cassidy@gmail.com>
2015-11-07 20:36:10 -0500

    Rejig rails sessions_controller output

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,20 +4,27 @@ skip_before_action :require_login
     u = User.find_by(username: params[:username])
     if u && u.authenticate(params[:password])
       session[:user_id] = u.id
-      h = Hash.new()
-      render json: {userId: u.id, username: current_user.username}
-    else
-      render json: {userId: 0}
     end
+    render_current_user
   end
 
   def show
-    render json: {userId: current_user.id, username: current_user.username}
+    render_current_user
   end
 
   def destroy
     session[:user_id] = nil
     redirect_to root_path
+  end
+
+  private
+
+  def render_current_user
+    if current_user
+      render json: {userId: current_user.id, username: current_user.username}
+    else
+      render json: {userId: 0, username: ""}
+    end
   end
 
 end
